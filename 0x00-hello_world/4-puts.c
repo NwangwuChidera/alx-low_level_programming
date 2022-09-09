a330ba64dcb9eac31c04d70256d49e14ff4d9914
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-08 18:26:56 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main- print the phrase "Programming is like building a multilingual puzzle"
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	puts("Programming is like builiding a multilingual puzzle\n");
+	return(0);
+}

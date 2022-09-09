1726618980b8f355028ece6564fe5dc9adfd7833
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-08 18:13:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#include <stdio.h>
-
-int main(void)
-{
-    puts("Programming is like building a multiligual puz          zle\n");
-    return(0);
-}    
---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main- print the phrase "Programminig is like building
+        a multilingual puzzle"
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	puts("Programming is like builing a multilingualpuzzle\n");
+	return(0):
+}		
+	

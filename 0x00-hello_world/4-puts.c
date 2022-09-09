490e8b826d4416dfce6587e6abb9ef332505ae5e
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-09 01:55:30 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - print the phrase "Programming is like building a multilingual puzzle"
- * Return: 0 when successful
+ * main -print the phrase "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("\"Programming is like building a multlingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

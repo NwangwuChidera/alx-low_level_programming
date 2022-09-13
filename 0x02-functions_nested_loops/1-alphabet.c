a05855136f7e9be659c3fafd7369124df33da249
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-13 14:13:49 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet -  prints the lowercase alphabet
+ * print_alphabet - prints the lowercase alphabet
  *
  * Return: void
  */
@@ -9,7 +9,7 @@ void print_alphabet(void)
 {
 	char c = 'a';
 	int i;
-	
+
 	for (i = 0; i < 26; i++)
 	{
 		_putchar(c + i);

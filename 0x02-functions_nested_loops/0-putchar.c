aa97079ecf1cd313c315cfe65d861c9030fb6f20
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-13 12:06:32 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-    char s[] = "_putchar";
-    int i = 0;
+	char s[] = "_putchar";
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-	    _putchar(s[i]);
-	    i++;
-    }
-    _putchar(10);
+	while (s[i] != '\0')
+	{
+		_putchar(s[i])
+		i++;
+	}
+	_putchar(10);
 
-    return (0);
+	return (0);
 }

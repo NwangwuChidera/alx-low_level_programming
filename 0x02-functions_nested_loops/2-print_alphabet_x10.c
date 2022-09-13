627be70fef265203eb6f69c888b458f3c994d2d1
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-13 14:53:39 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  * print_alphabet_x10 - prints the lowercase alphabet 10 times on new lines
  *
  * Return: void
- */
+*/
 void print_alphabet_x10(void)
 {
 	char c = 'a';
@@ -13,9 +13,9 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 26; j++)
-		{
-			_putchar(c + j)
-		}
-		_putchar(10);
+	{
+		_putchar(c + j);
+	}
+	_putchar(10)
 	}
 }

e8b32e9e0b9a6cfa6db3049f38afbe2f9c5d3402
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-15 12:18:20 -0700

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,13 +3,9 @@
  * mul - This function multiples two integer
  * @a: This is an integer character
  * @b: This is an integer character
- * Return: 0
+ * Return: result
  */
 int mul(int a, int b)
 {
-int a;
-int b;
-
-putchar ("%d a*b\n", a, b);
-return (0);
+return (a * b);
 }

7076c3ad0f6d55788cecd5515f2b82d3b1087f4e
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-15 13:49:38 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	int n;
-
-	for (n >= 48; n <= 57 n++)
-	{
-		_putchar(n);
-	}
-	_putchar(10);
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+_putchar(n);
+}
+_putchar(10);
 }

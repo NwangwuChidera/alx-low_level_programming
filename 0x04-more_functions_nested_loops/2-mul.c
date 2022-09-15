dc0f68d40435e38a3759b57a8be3f9163bc9fa11
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-15 12:12:56 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - This function multiples two integer
+ * @a: This is an integer character
+ * @b: This is an integer character
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+int a;
+int b;
+
+putchar ("%d a*b\n", a, b);
+return (0);
+}

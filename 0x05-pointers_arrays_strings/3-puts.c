23b972a4ad4acfc3290452e9b17613b2998b6b90
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-19 13:34:12 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - A function that prints a string followed by anew line
+ * @str: A string Character
+ * Return: The string
+ */
+void _puts(char *str)
+{
+	return (puts(str)"\n");
+}

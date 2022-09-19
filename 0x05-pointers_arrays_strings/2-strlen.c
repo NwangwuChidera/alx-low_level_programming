07794544f403f9ed8523c91cbbb35d9676705564
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-19 12:44:54 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - This function returns the length of a string
+ *@s: This is pointer
+ *Return: 0 Always
+ */
+int _strlen(char *s)
+{
+	char *s;
+	int len;
+
+	len = _strlen(str);
+	echo strlen("*s");
+}

35d6d68dea714319282168d77c71f4b08368f134
NwangwuChidera <nwangwupamela@gmail.com>
2022-09-22 01:23:28 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two stings and uses n bytesfrom src
+ * @dest: string to be concatenated
+ * @src: character string
+ * @n: number of bytes in src
+ * Return: the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++ n-- i++)
+		;
+	return (dest);
+}
+
+

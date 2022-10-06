5a04d46b7ebdd142f5f0c47985dbee8a019ac859
NwangwuChidera <nwangwupamela@gmail.com>
2022-10-06 12:21:36 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *malloc_checked - Write a function that allocates memory using malloc
+ * @b: integer allocated to the memory
+ * Return: Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	int n;
+	int i;
+
+	*b = malloc_checked(sizeof(int) * 98);
+	for (i = 0; i < n; i++)
+	{
+		b[i] = i + 1;
+	}
+	return (void);
+}
+

e9330f3d249987126dd9bc7e6402103e9e6af091
NwangwuChidera <nwangwupamela@gmail.com>
2022-10-20 11:34:34 -0700

    Task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,24 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * print_list - Prints all elements of a list
-  * @h: A linked list
-  *
-  * Return: The number of nodes
-  */
+ * print_list - prints list.
+ * @h: param list
+ * Return: struct
+ */
+
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t l_node;
 
+	l_node = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
 		h = h->next;
-		count++;
+		l_node++;
 	}
-
-	return (count);
+	return (l_node);
 }
--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,12 +16,11 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
-int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
